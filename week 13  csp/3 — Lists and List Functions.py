f9baf84edbe8a0268941d462d3c29d3b269fc9d4
Joanthan-09 <jonymn011@gmail.com>
2025-11-14 21:00:31 +0000

    Done

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -21,29 +21,74 @@ print (type(my_list)) #<class 'list>
 
 # Accessing elements
 
-print(my_list[0]) # 1
-print(my_list[1:4]) # [2, 3, 4]
-print(my_list[0:]) # [1, 2, 3, 4, 5]
+# print(my_list[0]) # 1
+# print(my_list[1:4]) # [2, 3, 4]
+# print(my_list[0:]) # [1, 2, 3, 4, 5]
 
-# Modifying list
-# Adding an item to the end of the list
+# # Modifying list
+# # Adding an item to the end of the list
 
-my_list.append(6)
-print(my_list) #[1, 2, 3, 4, 5, 6]
+# my_list.append(6)
+# print(my_list) #[1, 2, 3, 4, 5, 6]
 
-# You could do this but its time comsuming
-my_list.append(7)
-my_list.append(8)
-my_list.append(9)
-print(my_list) #[1, 2, 3, 4, 5, 6, 7, 8, 9]
+# # You could do this but its time comsuming
+# my_list.append(7)
+# my_list.append(8)
+# my_list.append(9)
+# print(my_list) #[1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-# the Extend command makes it efficent but could be time consuming 
-my_list.extend( [10,11,12,13,14,15] )
-print(my_list) 
+# # the Extend command makes it efficent but could be time consuming 
+# my_list.extend( [10,11,12,13,14,15] )
+# print(my_list) 
 
-# add 500 more numbers to the list (the most efficent)
-my_list.extend(list(range(15, 515)))
-print(my_list)
+# # add 500 more numbers to the list (the most efficent)
+# my_list.extend(list(range(15, 515)))
+# print(my_list)
+
+
+new_list = ['a', 'b', 'c']
+print(new_list) # [a, b, c]
+new_list.append('d')
+print(new_list) #[a, b, c, d]
+
+#removing an item from the list
+removed_item = new_list.pop(1)
+print(removed_item) # d 
+print(new_list) # [a, c, d]
+
+#sorting list
+numbers = [4, 2, 5, 1, 3]
+numbers.sort()
+print(numbers) # [1, 2, 3, 4, 5]
+# reverse list
+numbers.reverse()
+print(numbers) # [5, 4, 3 ,2 ,1 ]
+# insterting an item to a spefic position
+numbers.insert(2, 10)
+print(numbers) # [5, 4, 10, 3, 2, 1]
+third_list = [7, 8, 9]
+third_list[0] = 6
+print(third_list) # [6, 8, 9]
+third_list[-1] = 10
+print(third_list) # [6, 8, 10]
+
+import random 
+random_list = random.sample(range(1, 1000), 100) 
+# this will create a list of 10 unique random numbers 
+# between 1 & 999
+
+print(random_list)
+print(sorted(random_list))
+sorted_list = sorted(random_list)
+print(sorted_list)
+
+#   summary:
+# .append(item) - adds an item to the end of the list
+# .pop(index) - removes & returns the item at the specified index
+# .sort() - sorts the list in ascending order
+# .reverse() - reverses the order of the list 
+
+
 
 
 # Examples:
